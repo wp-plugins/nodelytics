c83d0dbf8e0bbba16e2b4ba3f6ec8f29363d4e99
straps <straps@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-18 13:55:29 +0000

    First Nodelytics Release with wp-admin integration
    git-svn-id: https://plugins.svn.wordpress.org/nodelytics/trunk@374285 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ nodelytics.php
@@ -0,0 +1,79 @@
+<?php
+/*
+Plugin Name: Nodelytics
+Plugin URI: http://www.strx.it/2011/04/nodelytics-node-js-real-time-stats/
+Description: Add Nodelytics code to enable real time statistics for your site
+Version: 1.0.0
+Author: Strx
+Author URI: http://www.strx.it
+License: GPL2
+*/
+
+function nodelytics_defaults(){
+    return array(
+  		//'findids'=>0
+    );
+}
+
+function nodelytics_get_options(){
+    $opts=nodelytics_defaults();
+    foreach($opts as $o=>$v){
+        $opts[$o]=get_option('nodelytics-'.$o, $v);
+    }
+    return $opts;
+}
+
+function nodelytics_start(){
+  $opts=nodelytics_get_options();
+	if (!is_user_logged_in()){
+		//$opts['outline']=0;
+	}
+  echo '<img src="http://nodelytics.strx.it/gif">';
+}
+
+function nodelytics_settings_menu(){
+    add_options_page(__('Nodelytics','strx'), __('Nodelytics','strx'), 'manage_options', 'nodelytics_settings', 'nodelytics_settings');
+}
+
+function nodelytics_settings(){
+    if (!current_user_can('manage_options')){ wp_die( __('You do not have sufficient permissions to access this page.') ); }
+
+    //Previous Saved Values or Default Ones
+    $opts=nodelytics_get_options();
+
+    //Update options
+    if( isset($_POST[ 'nodelytics-update' ]) ) {
+        foreach($opts as $o=>$v){
+            $opts[$o]=$_POST['nodelytics-'.$o];
+            update_option( 'nodelytics-'.$o, $opts[$o] );
+        }
+    }
+
+    extract($opts);
+
+	  $rv ='<div class="wrap">';
+    $rv.=' <h2>Nodelytics real time stats</h2>';
+    $rv.=' You can also access you Nodelytics at ';
+    
+    $home=get_bloginfo('home');
+    $urlInfo=parse_url($home);
+    $host=$urlInfo['host'];
+    
+    $nlurl="http://nodelytics.strx.it/stat/$host";
+
+	  $rv.='<a target="_blank" href="'.$nlurl.'">'.$nlurl.'</a>';
+	  $rv.='<iframe style="width: 100%; height: 560px; border:1px solid #aaa;" src="'.$nlurl.'" alt="Nodelytics for '.$host.'" title="Nodelytics for '.$host.'"></iframe>';
+    $rv.='</div>';
+
+    echo $rv;
+}
+
+
+//Registering scripts and plugin hooks
+if ( !is_admin() ) {
+    add_action('wp_footer','nodelytics_start');
+}else{
+    add_action('admin_menu', 'nodelytics_settings_menu');
+}
+
+
---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== Nodelytics Real Time Stats ===
+Contributors: strx
+Donate link: http://www.strx.it/donate
+Tags: analytics, statistics, access, spy
+Requires at least: 2.0
+Tested up to: 3.1.1
+Stable tag: 1.0.0
+
+Simplest way to add Nodelytics code to enable real time statistics
+
+== Description ==
+
+Nodelytics is a work-in-progress service that let you see in real time who is 
+visiting your web site without reloading the page.
+
+Youtube demo video: http://www.youtube.com/watch?v=Sxwp75HGHMs
+
+== Installation ==
+
+Upload the plugin to your blog, Activate it, Go to http://nodelytics.strx.it/stat/www.YOURSITE.com
+
+== Screenshots ==
+
+1. Nodelytics simple interface
+
+== Frequently Asked Questions ==
+
+= Where can I find more info or support =
+
+[Here](http://www.strx.it/2011/04/nodelytics-node-js-real-time-stats/).
+
+== Changelog ==
+
+= 1.0.0 =
+* First Version
